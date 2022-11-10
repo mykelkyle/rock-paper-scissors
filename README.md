a72f7b61ab33b804413eeb697f6385dc1f36a6ac
Mykel Murphy <murphy@akane.waseda.jp>
2022-11-10 11:40:06 +0900

    add a score tally and a way to announce winner

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # rock-paper-scissors
+
 The Odin Project Foundations (Rock Paper Scissors)
+
+In this project I implemented
+
+- for loops
+- functions & variables
+- retrieving a random item from an array
+- nested if conditionals
+
+Some challenges I faced
+
+- how to properly "nest" if conditionals and also forgetting the difference between assignment operators and comparison operators (= vs. == vs. ===)
+- figuring out how to make user input "case-insensitive" (.toLowerCase)
+- keeping track of the score and also announcing a winner at the end of the loop (concatenating the player score and the player score message)
--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+let playerScore = 0;
+let computerScore = 0;
+let playerScoreMessage = "Your score is: ";
+let computerScoreMessage = "The computer's score is: ";
+
+// loops through 5 rounds of RPS, keeps score, and reports a winner/loser at the end
+function game() {
+  for (let i = 0; i < 5; i++) {
+    const playerSelection = prompt("Rock, paper, or scissors?").toLowerCase();
+    const computerSelection = getComputerChoice();
+
+    console.log(renderGame(playerSelection, computerSelection));
+    console.log(playerScoreMessage + playerScore);
+    console.log(computerScoreMessage + computerScore);
+
+    if (i === 4) {
+      if (playerScore > computerScore) {
+        console.log(`The game has ended! You are the winner!`);
+      } else if (computerScore > playerScore) {
+        console.log(`The game has ended! The computer wins!`);
+      } else {
+        console.log(`The game has ended! It's a tie!`);
+      }
+      playerScore = 0;
+      computerScore = 0;
+    }
+  }
+}
+
 // returns a random item from array
 function getComputerChoice() {
   const choices = ["rock", "paper", "scissors"];
@@ -12,33 +41,30 @@ function renderGame(playerSelection, computerSelection) {
     return "It's a tie!";
   } else if (playerSelection == "rock") {
     if (computerSelection == "scissors") {
+      playerScore++;
       return "You win! Rock beats scissors.";
     } else {
+      computerScore++;
       return "You lose! Paper beats rock.";
     }
   } else if (playerSelection == "paper") {
     if (computerSelection == "rock") {
+      playerScore++;
       return "You win! Paper beats rock.";
     } else {
+      computerScore++;
       return "You lose! Scissors beat paper.";
     }
   } else if (playerSelection == "scissors") {
     if (computerSelection == "paper") {
+      playerScore++;
       return "You win! Scissors beat paper.";
     } else {
+      computerScore++;
       return "You lose! Rock beats scissors.";
     }
   } else {
     return "Syntax error. Check your spelling and try again!";
-  }
-}
-
-// loops through 5 rounds of RPS, keeps score, and reports a winner/loser at the end
-function game() {
-  for (let i = 0; i < 5; i++) {
-    const playerSelection = prompt("Rock, paper, or scissors?").toLowerCase();
-    const computerSelection = getComputerChoice();
-    console.log(renderGame(playerSelection, computerSelection));
   }
 }
 
